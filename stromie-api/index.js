da50c43e508e4e1498915b63afe95ddf3d3c207d
Abe Vriens <abe.vriens@gmail.com>
2021-11-03 15:37:29 +0100

    Update API

--- stromie-api/index.js
+++ stromie-api/index.js
@@ -7,23 +7,43 @@ let clients = [];
 
 app.use(express.json());
 
-app.post("/registerclient", (req, res) => {
+app.post("/register", (req, res) => {
     let json = req.body;
-    clients.push(json.clientId);
-    console.log("clients");
+    if(!json.hasOwnProperty('clientId')) {
+        res.status(400).json('Geen client ID opgegeven');
+    } else if(clients.includes(json.clientId)) {
+        res.status(406).json('Client ID al in gebruik.');
+    }
+    clients.push({
+        id: json.clientId,
+        ampere: 0
+    });
+    console.log(clients[0]);
 });
 
-app.get("/sendclient", (req, res) => {
-    axios.post('localhost/registerclient', {
-        clientId: 23
-      })
-      .then(res => {
-        console.log(`statusCode: ${res.status}`)
-        console.log(res)
-      })
-      .catch(error => {
-        console.error(error)
+app.post("/update", (req, res) => {
+    let json = req.body;
+    if(!json.hasOwnProperty('clientId') || !json.hasOwnProperty('ampere')) {
+        res.status(400).json('Geen client ID of ampere opgegeven');
+    } else if(!clients.includes(json.clientId)) {
+        res.status(406).json('Client ID niet herkend.');
+    }
+    let index = clients.findIndex(client => {
+        return client.id === json.clientId;
+    })
+
+    clients[index].ampere = json.ampere;
+});
+
+app.get("/", (req, res) => {
+    let json = req.body;
+    if(!clients.includes(json.id)) {
+        res.status(406).json('Client ID niet herkend.');
+    }
+    let index = clients.findIndex(client => {
+        return client.id === json.clientId;
     });
+    res.json = clients[index];
 });
 
 app.listen(port, () => {

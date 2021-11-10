dc67cadeb9d6f6287a0e02ec8a60a8a5d894442d
Abe <abe.vriens@gmail.com>
2021-11-10 12:13:27 +0100

    API communicatie met database compleet

--- stromie_arduino/src/main.cpp
+++ stromie_arduino/src/main.cpp
@@ -1,9 +1,82 @@
 #include <Arduino.h>
+#include "EmonLib.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <WiFiNINA.h>
+#include <ArduinoHttpClient.h>
+#include <ArduinoJson.h>
+#include <string.h>
 
-void setup() {
-  // put your setup code here, to run once:
+EnergyMonitor emon1;                   // Create an instance
+
+char SSID[] = "ArduinoWifi";
+char PASS[] = "0619H+i6";
+const char server[] = "145.93.100.136";
+
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, server, 2435);
+int status = WL_IDLE_STATUS;
+
+int clientId = 0;
+
+void registerArduino() {
+  StaticJsonDocument<8> json;
+
+  json["tafelNummer"] = 1;
+
+  String data;
+  serializeJson(json, data);
+
+  client.beginRequest();
+  client.post("/register", "application/json", data);
+
+  if(client.responseStatusCode() == 200) {
+    StaticJsonDocument<24> response;
+    deserializeJson(response, client.responseBody());
+    clientId = response["id"];
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void updateAmperage() {
+  StaticJsonDocument<16> json;
+
+  double amps = emon1.calcIrms(480);
+  Serial.println(amps);
+  json["clientId"] = clientId;
+  json["ampere"] = amps;
+
+  String data;
+  serializeJson(json, data);
+
+  client.beginRequest();
+  client.post("/update", "application/json", data);
+
+  if(client.responseStatusCode() == 406) {
+    registerArduino();
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  emon1.current(A0, 30);
+
+  while (status != WL_CONNECTED)
+  {
+    Serial.println("Connecting to WiFi...");
+    status = WiFi.begin(SSID, PASS);
+    delay(500);
+  }
+
+  Serial.println("Connected to the WiFi network");
+  Serial.println(WiFi.localIP());
+
+  registerArduino();
+}
+
+void loop()
+{
+  updateAmperage();
+  delay(1000);
 }
